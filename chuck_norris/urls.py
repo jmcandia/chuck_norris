f9fbdf11eb510d2d1fce05a76a47236df143ea88
José Miguel Candia <jmcandia@gmail.com>
2022-10-26 19:55:01 -0300

    Creación de la vista inicial

--- chuck_norris/urls.py
+++ chuck_norris/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from facts import views
 
 urlpatterns = [
+    path('', views.home, name='Home'),
     path('admin/', admin.site.urls),
 ]
--- facts/views.py
+++ facts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import random
+
+from django.shortcuts import HttpResponse
+
+facts = ["Hay 1424 cosas en una habitación promedio con las que Chuck Norris podría matarte. Incluyendo la habitación en sí.",
+         "Chuck Norris es la medida del sistema internacional del dolor.",
+         "Chuck Norris ganó un concurso sobre permanecer debajo del agua y ganó. Cabe destacar que su contrincante era pez.",
+         "Las lágrimas de Chuck Norris curan el cáncer. Lástima que jamás haya llorado.",
+         "Chuck Norris dona sangre a menudo. Pero rara vez es la suya.",
+         "La gente usa pijamas de Superman. Superman usa pijamas de Chuck Norris."]
 
 # Create your views here.
+
+
+def home(request):
+    current_fact = random.choice(facts)
+    content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Chuck Norris</title>
+    </head>
+    <body>
+        <p>Este es un hecho de Chuck Norris</p>
+        <blockquote>{current_fact}</blockquote>
+    </body>
+    </html>
+    """
+    return HttpResponse(content)
